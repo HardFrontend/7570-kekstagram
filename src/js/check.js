435766e26c94c4248ad8b727010c93f91c4b584e
Leiry <snt.evas@gmail.com>
2016-10-11 13:54:33 +0300

    #6 Начинаем программировать, правки

--- src/js/check.js
+++ src/js/check.js
@@ -19,21 +19,24 @@ window.getMessage = function (a, b) {
     return "Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
   }
 
-  else if (Array.isArray(a) === true) {
-    var amountOfRedPoints = 0;
-    for (var i = 0; i < a.length; i++) {
-      amountOfRedPoints += a[i];
-    }
-    return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
-  }
+  else if (Array.isArray(a)) {
 
-  else if (Array.isArray(a) === true && Array.isArray(b) === true) {
-    var artifactsSquare = 0;
-    for (var i = 0; i < a.length; i++) {
-      var c = a[i] * b[i];
-      artifactsSquare += c;
+   if ( Array.isArray(b)) {
+      var artifactsSquare = 0;
+      for (var i = 0; i < a.length; i++) {
+        var c = a[i] * b[i];
+        artifactsSquare += c;
+      }
+      return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
     }
-    return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+
+    else {
+      var amountOfRedPoints = 0;
+      for (var i = 0; i < a.length; i++) {
+        amountOfRedPoints += a[i];
+      }
+      return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+      }
   }
 
   else {
